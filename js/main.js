7a86db1c5ff88bea317d85167ca75374141836c1
Laura Caldaronello <caldaronello.l@gmail.com>
2021-03-05 15:29:45 +0100

    Passo a caso

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
+// cerco di generalizzare
 var nImgs = $('.img-container').children().length;
 console.log(nImgs);
+
+var cLeft = $('#top .left-container');
+var cRight = $('#top .right-container');
+
+cRight.click( function () {
+
+    var imgActive = $('img.active');
+    imgActive.removeClass('active');
+    
+    if (imgActive.hasClass('last')) {
+        var imgFirst = $('img.first');
+        imgFirst.addClass('active');
+    }
+    else {
+        imgActive.next().addClass('active');
+    }
+
+});
