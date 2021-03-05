7a7f6539bf3a78888abd74d8409b9950f30b65da
Laura Caldaronello <caldaronello.l@gmail.com>
2021-03-05 16:43:52 +0100

    Non ho ancora finito

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
-// cerco di generalizzare
-var nImgs = $('.img-container').children().length;
-console.log(nImgs);
+// cerco di generalizzare, così se voglio aggiungere immagini devo semplicemente farlo nell'html
+var imgs = $('.img-container');
 
-var cLeft = $('#top .left-container');
+imgs.children().first().addClass(['first','active']);
+imgs.children().last().addClass('last');
+
+// parte chevron
+
 var cRight = $('#top .right-container');
-
 cRight.click( function () {
-
+    
     var imgActive = $('img.active');
     imgActive.removeClass('active');
     
@@ -17,5 +19,48 @@ cRight.click( function () {
     else {
         imgActive.next().addClass('active');
     }
-
+    
 });
+
+var cLeft = $('#top .left-container');
+cLeft.click( function () {
+    
+    var imgActive = $('img.active');
+    imgActive.removeClass('active');
+    
+    if (imgActive.hasClass('first')) {
+        var imgLast = $('img.last');
+        imgLast.addClass('active');
+    }
+    else {
+        imgActive.prev().addClass('active');
+    }
+    
+});
+
+// /parte chevron
+
+// parte pallini
+
+// cerco di generalizzare: li aggiungo e gestisco in automatico all'aumentare delle img
+var bottom = $('#bottom');
+for (var i = 0; i < imgs.children().length; i++) {
+    bottom.append(' <i class="fas fa-circle"></i>');
+}
+
+var circle = $('#bottom > *');
+console.log(circle[0]);
+for (var i = 0; i < imgs.children().length; i++) {
+
+    circle[i].click( function () {
+    
+        var imgActive = $('img.active');
+        imgActive.removeClass('active');
+        
+        imgs.children([i]).addClass('active');
+        
+    });
+
+}
+
+// /parte pallini
