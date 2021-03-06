3fa85eb9dafe120616ad089d999fbbf2522f38e4
Laura Caldaronello <caldaronello.l@gmail.com>
2021-03-06 14:26:44 +0100

    Ok comandi tastiera

--- js/main.js
+++ js/main.js
@@ -1,30 +1,11 @@
 // cerco di generalizzare, così se voglio aggiungere immagini devo semplicemente farlo nell'html
-var imgs = $('.img-container');
+var imgs = $('.img-container > *');
 
-imgs.children().first().addClass(['first','active']);
-imgs.children().last().addClass('last');
+imgs.first().addClass(['first','active']);
+imgs.last().addClass('last');
 
-// parte chevron
+function goToLeftGeneral() {
 
-var cRight = $('#top .right-container');
-cRight.click( function () {
-    
-    var imgActive = $('img.active');
-    imgActive.removeClass('active');
-    
-    if (imgActive.hasClass('last')) {
-        var imgFirst = $('img.first');
-        imgFirst.addClass('active');
-    }
-    else {
-        imgActive.next().addClass('active');
-    }
-    
-});
-
-var cLeft = $('#top .left-container');
-cLeft.click( function () {
-    
     var imgActive = $('img.active');
     imgActive.removeClass('active');
     
@@ -35,32 +16,80 @@ cLeft.click( function () {
     else {
         imgActive.prev().addClass('active');
     }
-    
-});
 
-// /parte chevron
+}
+var goToLeftKey = function(event) {
+    if (event.which == 37) {
+        goToLeftGeneral();
+    }
+}
+
+function goToRightGeneral() {
+    var imgActive = $('img.active');
+    imgActive.removeClass('active');
+    
+    if (imgActive.hasClass('last')) {
+        var imgFirst = $('img.first');
+        imgFirst.addClass('active');
+    }
+    else {
+        imgActive.next().addClass('active');
+    }
+}
+var goToRightKey = function(event) {
+    if (event.which == 39) {
+        goToRightGeneral();
+    }
+}
+
+// parte chevron e keyboard
+
+var cRight = $('#top .right-container');
+cRight.click(goToRightGeneral);
+$(document).keydown(goToRightKey);
+
+
+var cLeft = $('#top .left-container');
+cLeft.click(goToLeftGeneral);
+$(document).keydown(goToLeftKey);
+
+// /parte chevron e keyboard
 
 // parte pallini
 
 // cerco di generalizzare: li aggiungo e gestisco in automatico all'aumentare delle img
 var bottom = $('#bottom');
-for (var i = 0; i < imgs.children().length; i++) {
+for (var i = 0; i < imgs.length; i++) {
     bottom.append(' <i class="fas fa-circle"></i>');
 }
 
 var circle = $('#bottom > *');
 console.log(circle[0]);
-for (var i = 0; i < imgs.children().length; i++) {
+console.log(imgs[0]);
+
+
+for (let i = 0; i < imgs.length; i++) {
 
     circle[i].click( function () {
     
         var imgActive = $('img.active');
         imgActive.removeClass('active');
         
-        imgs.children([i]).addClass('active');
+        imgs[i].addClass('active');
         
     });
 
 }
+
+circle[1].click( function () {
+    
+    // var imgActive = $('img.active');
+    // imgActive.removeClass('active');
+    
+    // imgs[1].addClass('active');
+    alert('ciao');
+    
+});
+
 
 // /parte pallini
